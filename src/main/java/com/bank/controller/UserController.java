3196962b80990bb5768d1151f028ab965c73d926
Murat Çakıroğlu <mcakiroglu.techproed@gmail.com>
2021-08-05 20:36:14 +0300

    v1

--- src/main/java/com/bank/controller/UserController.java
+++ src/main/java/com/bank/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://www.htrbank.com", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/user")
 public class UserController {
