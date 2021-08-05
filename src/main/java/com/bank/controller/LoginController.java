1f3c2357e155ebc9b6d2a1384f628a2584d6af06
Murat Çakıroğlu <mcakiroglu.techproed@gmail.com>
2021-08-06 01:25:15 +0300

    day18

--- src/main/java/com/bank/controller/LoginController.java
+++ src/main/java/com/bank/controller/LoginController.java
@@ -63,12 +63,12 @@ public class LoginController {
         if(userRepo.existsByUsername(signUpForm.getUsername())){
             response.setMessage("Error: Username is already taken");
             response.setSuccess(false);
-            return new ResponseEntity<>(response, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
         if(userRepo.existsByEmail(signUpForm.getEmail())){
             response.setMessage("Error: Email is already taken");
             response.setSuccess(false);
-            return new ResponseEntity<>(response, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
         //Create a new user
          User user = new User(
