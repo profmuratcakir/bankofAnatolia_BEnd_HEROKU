418ad94a82a2c75104795979197506d4d8c61675
Murat Çakıroğlu <mcakiroglu.techproed@gmail.com>
2021-08-05 18:57:53 +0300

    v1

--- src/main/java/com/bank/BankOfAnatoliaApplication.java
+++ src/main/java/com/bank/BankOfAnatoliaApplication.java
@@ -1,7 +1,9 @@
 package com.bank;
 
 import com.bank.model.Role;
+import com.bank.model.User;
 import com.bank.repository.RoleRepo;
+import com.bank.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -23,6 +25,9 @@ class DemoCommandLineRunner implements CommandLineRunner {
 	@Autowired
 	private RoleRepo roleRepo;
 
+	@Autowired
+	private UserRepo userRepo;
+
 	@Override
 	public void run(String... args) throws Exception {
 		Role roleAdmin = new Role();
@@ -32,5 +37,9 @@ class DemoCommandLineRunner implements CommandLineRunner {
 		Role roleUser = new Role();
 		roleUser.setName("USER");
 		roleRepo.save(roleUser);
+
+		User user1 = new  User("canan","123456", "Canan", "OZ","c@gmail.com");
+		userRepo.save(user1);
+
 	}
 }
