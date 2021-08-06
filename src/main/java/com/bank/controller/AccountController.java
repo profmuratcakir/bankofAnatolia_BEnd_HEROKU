947c0ea0f64210d1afb335c15f8f2aa40773ba6f
Murat Çakıroğlu <mcakiroglu.techproed@gmail.com>
2021-08-06 12:36:26 +0300

    day19

--- src/main/java/com/bank/controller/AccountController.java
+++ src/main/java/com/bank/controller/AccountController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "https://www.htrbank.com", allowedHeaders = "*")
 @RestController
 @RequestMapping("/account")
 public class AccountController {
