e5ecd4ae139b2d108f603addaa6fb67cb42785bf
Murat Çakıroğlu <mcakiroglu.techproed@gmail.com>
2021-08-07 20:02:42 +0300

    day19

--- src/main/java/com/bank/config/CorsConfiguration.java
+++ src/main/java/com/bank/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*").
+        registry.addMapping("/**").
                 allowedOrigins("https://www.htrbank.com").
                 allowedOrigins("https://bankofanatolia.netlify.app");
     }
