b74974504088d8c69a342e785f8ab449072f2297
Murat Çakıroğlu <mcakiroglu.techproed@gmail.com>
2021-08-08 16:43:31 +0300

    day20

--- src/main/java/com/bank/config/CorsConfiguration.java
+++ src/main/java/com/bank/config/CorsConfiguration.java
@@ -1,19 +1,18 @@
-//package com.bank.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class CorsConfiguration implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").
-//                allowedHeaders("*").
-//                allowedMethods("*").
-//                allowedOrigins("https://www.htrbank.com").
-//                allowedOrigins("https://bankofanatolia.netlify.app");
-//    }
-//}
+package com.bank.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").
+                allowedHeaders("*").
+                allowedOrigins("https://www.htrbank.com").
+                allowedOrigins("https://bankofanatolia.netlify.app");
+    }
+}
